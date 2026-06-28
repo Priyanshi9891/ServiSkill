5a6747312cd4366c381c5943788e1959dd295153
Priyanshi9891 <priyumaurya02@gmail.com>
2026-06-28 19:34:06 +0530

    Fix useSearchParams suspense error

--- app/bookings/create/page.js
+++ app/bookings/create/page.js
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import CreateBookingContent from "./CreateBookingContent";
-
+export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
